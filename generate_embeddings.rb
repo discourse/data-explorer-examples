419e28620e70c7fc2747188336a490e8a8ec6b1e
Sam Saffron <sam.saffron@gmail.com>
2023-08-10 10:16:09 +1000

    aider: Add `generate_embeddings.rb` script to call OpenAI API and generate embeddings for all examples.
    # Aider chat conversation:
    
    USER: write "generate_embeddings.rb" it will call openai and generate embeddings for all the examples
    
    ASSISTANT: Sure, I can help you create a `generate_embeddings.rb` script. However, I need more information about how you want to use OpenAI to generate embeddings. Could you please provide more details? For example, what OpenAI model do you want to use, and what kind of embeddings do you want to generate?
    
    USER: just translate this curl pattern to ruby curl https://api.openai.com/v1/embeddings \
      -H "Content-Type: application/json" \
      -H "Authorization: Bearer $OPENAI_API_KEY" \
      -d '{
        "input": "Your text string goes here",
        "model": "text-embedding-ada-002"
      }'
    
    ASSISTANT: Sure, I can help you with that. We will use the `net/http` and `json` libraries in Ruby to make the HTTP request. Here is the `generate_embeddings.rb` script:
    
    ```ruby
    generate_embeddings.rb
    <<<<<<< ORIGINAL
    
    =======
    require 'net/http'
    require 'json'
    require 'uri'
    
    # Define the API endpoint
    uri = URI.parse("https://api.openai.com/v1/embeddings")
    
    # Define the request
    request = Net::HTTP::Post.new(uri)
    request.content_type = "application/json"
    request["Authorization"] = "Bearer #{ENV['OPENAI_API_KEY']}"
    Dir.glob('examples/*.txt').each do |file|
      text = File.read(file)
      request.body = JSON.dump({
        "input" => text,
        "model" => "text-embedding-ada-002"
      })
    
      # Send the request
      response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
        http.request(request)
      end
    
      # Print the response
      puts response.body
    end
    >>>>>>> UPDATED
    ```
    
    This script reads each text file in the `examples` directory, sends the content of the file to the OpenAI API, and prints the response. It uses the `OPENAI_API_KEY` environment variable for authentication.
    
    Please replace `text-embedding-ada-002` with the actual name of your model if it's different.
    
    To run the script, you would use the following command:
    
    ```bash
    ruby generate_embeddings.rb
    ```
    
    This will generate ADA embeddings for all text files in the `examples` directory and print the embeddings.

---
+++ generate_embeddings.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+# Define the API endpoint
+uri = URI.parse("https://api.openai.com/v1/embeddings")
+
+# Define the request
+request = Net::HTTP::Post.new(uri)
+request.content_type = "application/json"
+request["Authorization"] = "Bearer #{ENV['OPENAI_API_KEY']}"
+Dir.glob('examples/*.txt').each do |file|
+  text = File.read(file)
+  request.body = JSON.dump({
+    "input" => text,
+    "model" => "text-embedding-ada-002"
+  })
+
+  # Send the request
+  response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+
+  # Print the response
+  puts response.body
+end
