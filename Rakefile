095cc54ae27c7cf5cb61bafbea6f22fe88563944
Sam Saffron <sam.saffron@gmail.com>
2023-08-10 10:34:59 +1000

    wip

--- Rakefile
+++ Rakefile
@@ -1,8 +1,8 @@
-require 'rake'
+require "rake"
 
 namespace :ada do
-  desc 'Generate ADA embeddings for all text files in the examples directory'
+  desc "Generate ADA embeddings for all text files in the examples directory"
   task :generate_embeddings do
-    system("ruby generate_embeddings.rb")
+    system("ruby scripts/generate_embeddings.rb")
   end
 end
--- scripts/generate_embeddings.rb
+++ scripts/generate_embeddings.rb
@@ -1,6 +1,9 @@
-require 'net/http'
-require 'json'
-require 'uri'
+require "net/http"
+require "json"
+require "uri"
+
+EMBEDDINGS_DIR = File.join(__dir__, "../examples/embeddings")
+EXAMPLES_DIR = File.join(__dir__, "../examples")
 
 # Define the API endpoint
 uri = URI.parse("https://api.openai.com/v1/embeddings")
@@ -8,36 +11,46 @@ uri = URI.parse("https://api.openai.com/v1/embeddings")
 # Define the request
 request = Net::HTTP::Post.new(uri)
 request.content_type = "application/json"
-request["Authorization"] = "Bearer #{ENV['OPENAI_API_KEY']}"
-Dir.glob('examples/*.txt').each do |file|
-  description = File.open(file, &:readline).strip
-  request.body = JSON.dump({
-    "input" => description,
-    "model" => "text-embedding-ada-002"
-  })
+request["Authorization"] = "Bearer #{ENV["OPENAI_API_KEY"]}"
+Dir
+  .glob("#{EXAMPLES_DIR}/*.txt")
+  .each do |file|
+    description = File.open(file, &:readline).strip
 
-  # Send the request
-  response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-    http.request(request)
-  end
+    sha = Digest::SHA256.hexdigest(description)
 
-  # Generate SHA256 hash of the description
-  sha = Digest::SHA256.hexdigest(description)
-  
-  # Check if a file with the same SHA256 hash already exists
-  if File.exist?("embeddings/#{sha}.json")
-    puts "Embedding for #{file} already exists. Skipping..."
-  else
-    puts "Generating embedding for #{file}..."
-    
-    # Send the request
-    response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-      http.request(request)
+    embed_filename = "embeddings/#{File.basename(file, ".txt")}.json"
+
+    existing_sha =
+      begin
+        JSON.parse(File.read(embed_filename))["sha"]
+      rescue StandardError
+        nil
+      end
+
+    if existing_sha == sha
+      puts "Embedding for #{file} already exists. Skipping..."
+    else
+      puts "Generating embedding for #{file}..."
+
+      request.body =
+        JSON.dump(
+          { "input" => description, "model" => "text-embedding-ada-002" }
+        )
+
+      response =
+        Net::HTTP.start(
+          uri.hostname,
+          uri.port,
+          use_ssl: uri.scheme == "https"
+        ) { |http| http.request(request) }
+
+      File.open(embed_filename, "w") do |f|
+        f.write(
+          JSON.pretty_generate(
+            JSON.parse(response.body).merge({ "sha" => sha })
+          )
+        )
+      end
     end
-
-    # Save the response to a JSON file
-    File.open("embeddings/#{File.basename(file, '.txt')}.json", 'w') do |f|
-      f.write(JSON.pretty_generate(JSON.parse(response.body).merge({"sha" => sha})))
-    end
   end
-end
